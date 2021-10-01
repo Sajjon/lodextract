537b8a0d4dfaa326f787214c268ef2427633dc64
Alexander Cyon <alex.cyon@gmail.com>
2021-10-01 10:13:08 +0200

    Upgrade to Python 3.8. Modify script to write SHA256 hashes of pixeldata of DEF files from H3MSprite.lod archive (hard coded only this LOD archive supported now). Needs cleanup to remove hardcoded values

--- defextract.py
+++ defextract.py
@@ -21,72 +21,129 @@
 # vcmi/client/CAnimation.cpp
 
 import struct
+from io import RawIOBase
+
 from PIL import Image, ImageDraw
 from collections import defaultdict
 import os
 import json
 import numpy as np
 
-def extract_def(infile,outdir):
+import hashlib
+
+class Block:
+
+    def __init__(self, frameNames, offsets):
+        assert(len(frameNames) == len(offsets))
+        all(isinstance(n, str) for n in frameNames)
+        self.frameNames = frameNames
+        all(isinstance(n, int) for n in offsets)
+        self.offsets = offsets
+        self.frameCount = len(frameNames)
+
+# class Frame:
+#
+#     def __init__(self, name: str):
+#         self.name = name
+#         self.offset = None
+#
+#     def setOffset(self, offset: int):
+#         self.offset = offset
+
+
+def sha256(data):
+    return hashlib.sha256(data).hexdigest()
+
+def extract_def(infile):
     f = open(infile)
-    bn = os.path.basename(infile)
-    bn = os.path.splitext(bn)[0].lower()
+    deffilename = os.path.basename(infile)
+    bn = os.path.splitext(deffilename)[0].lower()
+    outdir = "/Users/sajjon/Library/Application Support/Makt/Temp/def_file_entries_from_H3sprite_lod/"
+    extract_def_stream(f, deffilename, outdir)
+
+def extract_def_stream(stream: RawIOBase, deffilename: str, outdir: str):
+    bn = deffilename
+    f = stream
+
+    outpath = os.path.join(outdir,deffilename)
+    if os.path.exists(outpath):
+        if not os.path.isdir(outpath):
+            print("output path exists and is no directory")
+            return False
+    else:
+        os.mkdir(outpath)
+
 
     # t - type
     # blocks - # of blocks
     # the second and third entry are width and height which are not used
-    t,_,_,blocks = struct.unpack("<IIII", f.read(16))
+    t,_,_,blockCount = struct.unpack("<IIII", f.read(16))
 
     palette = []
     for i in range(256):
         r,g,b = struct.unpack("<BBB", f.read(3))
         palette.extend((r,g,b))
 
-    offsets = defaultdict(list)
-    for i in range(blocks):
-        # bid - block id
+    blocksByID = defaultdict()
+    for blockIndex in range(blockCount):
         # entries - number of images in this block
         # the third and fourth entry have unknown meaning
-        bid,entries,_,_ = struct.unpack("<IIII", f.read(16))
-        names=[]
+        blockId,frameCount,_,_ = struct.unpack("<IIII", f.read(16))
+        frameNames=[]
         # a list of 13 character long filenames
-        for j in range(entries):
-            name, = struct.unpack("13s", f.read(13))
-        # a list of offsets
-        for j in range(entries):
-            offs, = struct.unpack("<I", f.read(4))
-            offsets[bid].append(offs)
+        for frameIndex in range(frameCount):
+            frameNameRaw, = struct.unpack("13s", f.read(13))
+            frameName = frameNameRaw.decode('utf8')
+            frameNameNonNull = frameName
+            if '\x00' in frameName:
+                frameNameNonNull = frameName.strip().strip('\x00')
+            frameNames.append(frameNameNonNull)
 
-    outpath = os.path.join(outdir,"%s.dir"%bn)
-    if os.path.exists(outpath):
-        if not os.path.isdir(outpath):
-            print "output path exists and is no directory"
-            return False
-    else:
-        os.mkdir(outpath)
+        # a list of offsets
+        offsetList=[]
+        for frameIndex in range(frameCount):
+            frameOffset, = struct.unpack("<I", f.read(4))
+            offsetList.append(frameOffset)
+
+
+        blocksByID[blockId] = Block(frameNames, offsetList)
 
     out_json = {"sequences":[],"type":t,"format":-1}
+    out_hashes_json = { "defFileName": deffilename, "hashesOfFrames": []  }
 
     firstfw,firstfh = -1,-1
-    for bid,l in offsets.items():
+    globalFrameId = -1
+
+    for blockid,block in blocksByID.items():
+        bid = blockid
         frames=[]
-        for j,offs in enumerate(l):
-            f.seek(offs)
-            pixeldata = ""
+        for frameid in range(block.frameCount):
+            globalFrameId += 1
+            j = frameid
+            frameOffset = block.offsets[frameid]
+            framename = block.frameNames[frameid]
+            f.seek(frameOffset)
+            pixeldata = b""
             # first entry is the size which is unused
             # fmt - encoding format of image data
             # fw,fh - full width and height
             # w,h - width and height, w must be a multiple of 16
             # lm,tm - left and top margin
             _,fmt,fw,fh,w,h,lm,tm = struct.unpack("<IIIIIIii", f.read(32))
-            outname = os.path.join(outdir,"%s.dir"%bn,"%02d_%02d.png"%(bid,j))
-            print "writing to %s"%outname
+
+            outname = os.path.join(
+                outdir,
+                deffilename,
+                "%s.png" % framename
+            )
+
+            out_hash_json = {"frameName": framename, "globalFrameId": globalFrameId, "blockId": blockid, "frameIdInBlock": frameid, "hash": "not_set"}
 
             # SGTWMTA.def and SGTWMTB.def fail here
             # they have inconsistent left and top margins
             # they seem to be unused
             if lm > fw or tm > fh:
-                print "margins (%dx%d) are higher than dimensions (%dx%d)"%(lm,tm,fw,fh)
+                print("margins (%dx%d) are higher than dimensions (%dx%d)"%(lm,tm,fw,fh))
                 return False
 
             if firstfw==-1 and firstfh == -1:
@@ -94,22 +151,22 @@ def extract_def(infile,outdir):
                 firstfh = fh
             else:
                 if firstfw > fw:
-                    print "must enlarge width"
+                    print("must enlarge width")
                     fw = firstfw
                 if firstfw < fw:
-                    print "first with smaller than latter one"
+                    print("first with smaller than latter one")
                     return False
                 if firstfh > fh:
-                    print "must enlarge height"
+                    print("must enlarge height")
                     fh = firstfh
                 if firstfh < fh:
-                    print "first height smaller than latter one"
+                    print("first height smaller than latter one")
                     return False
 
             if out_json["format"] == -1:
                 out_json["format"] = fmt
             elif out_json["format"] != fmt:
-                print "format %d of this frame does not match of last frame %d"%(fmt,global_fmt)
+                print("format %d of this frame does not match of last frame %d"%(fmt,global_fmt))
                 return False
 
             frames.append(os.path.join("%s.dir"%bn,"%02d_%02d.png"%(bid,j)))
@@ -120,7 +177,7 @@ def extract_def(infile,outdir):
                 elif fmt == 1:
                     lineoffs = struct.unpack("<"+"I"*h, f.read(4*h))
                     for lineoff in lineoffs:
-                        f.seek(offs+32+lineoff)
+                        f.seek(frameOffset+32+lineoff)
                         totalrowlength=0
                         while totalrowlength<w:
                             code,length = struct.unpack("<BB", f.read(2))
@@ -134,9 +191,9 @@ def extract_def(infile,outdir):
                     lineoffs = struct.unpack("<%dH"%h, f.read(2*h))
                     _,_ = struct.unpack("<BB", f.read(2)) # unknown
                     for lineoff in lineoffs:
-                        if f.tell() != offs+32+lineoff:
-                            print "unexpected offset: %d, expected %d"%(f.tell(),offs+32+lineoff)
-                            f.seek(offs+32+lineoff)
+                        if f.tell() != frameOffset+32+lineoff:
+                            # print("unexpected offset: %d, expected %d"%(f.tell(),frameOffset+32+lineoff))
+                            f.seek(frameOffset+32+lineoff)
                         totalrowlength=0
                         while totalrowlength<w:
                             segment, = struct.unpack("<B", f.read(1))
@@ -145,7 +202,7 @@ def extract_def(infile,outdir):
                             if code == 7: # raw data
                                 pixeldata += f.read(length)
                             else: # rle
-                                pixeldata += length*chr(code)
+                                pixeldata += (length*chr(code)).encode('utf8')
                             totalrowlength+=length
                 elif fmt == 3:
                     # each row is split into 32 byte long blocks which are individually encoded
@@ -154,9 +211,9 @@ def extract_def(infile,outdir):
 
                     for lineoff in lineoffs:
                         for i in lineoff:
-                            if f.tell() != offs+32+i:
-                                print "unexpected offset: %d, expected %d"%(f.tell(),offs+32+i)
-                                f.seek(offs+32+i)
+                            if f.tell() != frameOffset+32+i:
+                                # print("unexpected offset: %d, expected %d"%(f.tell(),frameOffset+32+i))
+                                f.seek(frameOffset+32+i)
                             totalblocklength=0
                             while totalblocklength<32:
                                 segment, = struct.unpack("<B", f.read(1))
@@ -168,9 +225,11 @@ def extract_def(infile,outdir):
                                     pixeldata += length*chr(code)
                                 totalblocklength+=length
                 else:
-                    print "unknown format: %d"%fmt
+                    print("unknown format: %d"%fmt)
                     return False
-                imp = Image.fromstring('P', (w,h),pixeldata)
+
+                imp = Image.frombytes('P', (w,h),pixeldata) # PIL.Image.frombuffer(mode, size, data, decoder_name='raw', *args)[source]
+
                 imp.putpalette(palette)
                 # convert to RGBA
                 imrgb = imp.convert("RGBA")
@@ -197,17 +256,27 @@ def extract_def(infile,outdir):
             else: # either width or height is zero
                 im = Image.new('RGBA', (fw,fh), (0,0,0,0))
             im.save(outname)
+
+            pixeldata_sha256 = sha256(pixeldata)
+            out_hash_json["hash"] = pixeldata_sha256
+            out_hashes_json["hashesOfFrames"].append(out_hash_json)
         out_json["sequences"].append({"group":bid,"frames":frames})
         with open(os.path.join(outdir,"%s.json"%bn),"w+") as o:
             json.dump(out_json,o,indent=4)
-    return True
+        with open(os.path.join(outdir, "%s_outhash.json"%deffilename), "w+") as o:
+            json.dump(out_hashes_json, o, indent=4)
+    return out_hashes_json
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) != 3:
-        print "usage: %s input.def ./outdir"%sys.argv[0]
-        print "to process all files:"
-        print "    for f in *.def; do n=`basename $f .def`; mkdir -p defs/$n; %s defextract.py $f defs/$n; done"%sys.argv[0]
-        exit(1)
-    ret = extract_def(sys.argv[1], sys.argv[2])
+    # if len(sys.argv) != 3:
+    #     print "usage: %s input.def ./outdir"%sys.argv[0]
+    #     print "to process all files:"
+    #     print "    for f in *.def; do n=`basename $f .def`; mkdir -p defs/$n; %s defextract.py $f defs/$n; done"%sys.argv[0]
+    #     exit(1)
+    # ret = extract_def(sys.argv[1], sys.argv[2])
+    infile = "/Users/sajjon/Library/Application Support/Makt/Data/H3sprite.lod"
+    print("WARNING hardcoded to extract def files in lod %s" % infile)
+
+    ret = extract_def(infile)
     exit(0 if ret else 1)
--- lodextract.py
+++ lodextract.py
@@ -19,7 +19,11 @@
 import zlib
 import struct
 import os
+import json
 from PIL import Image, ImageDraw
+
+from defextract import extract_def_stream
+
 
 def is_pcx(data):
     size,width,height = struct.unpack("<III",data[:12])
@@ -41,12 +45,14 @@ def read_pcx(data):
     else:
         return None
 
-def unpack_lod(infile,outdir):
-    f = open(infile)
-
+def unpack_lod(infile,outdir, outdirOfHashesFile: str, defFilesOfInterest):
+    all(isinstance(n, str) for n in defFilesOfInterest)
+    f = open(infile, 'rb')
+    archiveFileName = os.path.basename(infile)
+    print("f.name='%s'" % f.name)
     header = f.read(4)
-    if header != 'LOD\0':
-        print "not LOD file: %s"%header
+    if not header.startswith(b'LOD'):
+        print("not LOD file: '%s'"%header)
         return False
 
     f.seek(8)
@@ -56,13 +62,19 @@ def unpack_lod(infile,outdir):
     files=[]
     for i in range(total):
         filename, = struct.unpack("16s", f.read(16))
-        filename = filename[:filename.index('\0')].lower()
+        filename = str(filename[:filename.index(b'\0')], 'utf8') # .lower()
         offset,size,_,csize = struct.unpack("<IIII", f.read(16))
         files.append((filename,offset,size,csize))
 
+
+    out_json = {"archive": archiveFileName, "hashesOfDefFiles": []}
+
     for filename,offset,size,csize in files:
+        deffilename = filename
         filename=os.path.join(outdir,filename)
-        print filename
+        # print(filename)
+        # print("type of variable 'filename' is: '%s'" % (type(filename)))
+        # exit(1)
         f.seek(offset)
         if csize != 0:
             data = zlib.decompress(f.read(csize))
@@ -76,21 +88,70 @@ def unpack_lod(infile,outdir):
             filename = filename+".png"
             im.save(filename)
         else:
-            with open(filename,"w+") as o:
+            with open(filename,"wb+") as o:
                 o.write(data)
+
+            lowers = defFilesOfInterest
+            for i in range(len(lowers)):
+                lowers[i] = lowers[i].lower()
+
+            if deffilename.lower() in lowers:
+                print("Found def file of interest: '%s'" % deffilename)
+                with open(filename, "rb") as df:
+                    hashes_json_of_def_file = extract_def_stream(df, deffilename, "/Users/sajjon/Library/Application Support/Makt/Temp/def_file_entries_from_H3sprite_lod/")
+                    out_json["hashesOfDefFiles"].append(hashes_json_of_def_file)
+
+    with open(os.path.join(outdirOfHashesFile, "%s_hashes_of_def_files.json" % archiveFileName), "w+") as o:
+        json.dump(out_json, o, indent=4)
 
     return True
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) != 3:
-        print "usage: %s infile.lod ./outdir"%sys.argv[0]
-        print ""
-        print "usually after installing the normal way:"
-        print "    %s .vcmi/Data/H3bitmap.lod .vcmi/Mods/vcmi/Data/"%sys.argv[0]
-        print "    rm .vcmi/Data/H3bitmap.lod"
-        print "    %s .vcmi/Data/H3sprite.lod .vcmi/Mods/vcmi/Data/"%sys.argv[0]
-        print "    rm .vcmi/Data/H3sprite.lod"
-        exit(1)
-    ret = unpack_lod(sys.argv[1], sys.argv[2])
+    # import sys
+    # if len(sys.argv) != 3:
+    #     print "usage: %s infile.lod ./outdir"%sys.argv[0]
+    #     print ""
+    #     print "usually after installing the normal way:"
+    #     print "    %s .vcmi/Data/H3bitmap.lod .vcmi/Mods/vcmi/Data/"%sys.argv[0]
+    #     print "    rm .vcmi/Data/H3bitmap.lod"
+    #     print "    %s .vcmi/Data/H3sprite.lod .vcmi/Mods/vcmi/Data/"%sys.argv[0]
+    #     print "    rm .vcmi/Data/H3sprite.lod"
+    #     exit(1)
+    # ret = unpack_lod(sys.argv[1], sys.argv[2])
+
+    infile = "/Users/sajjon/Library/Application Support/Makt/Data/H3sprite.lod"
+    outdir = "/Users/sajjon/Library/Application Support/Makt/Temp/def__files_from__H3sprite_lod/"
+    outdirOfHashesFile = "/Users/sajjon/Library/Application Support/Makt/Temp/hashesOfDefFilesInArchive/"
+    print("WARNING hardcoded to extract def files in lod at path\n'%s'\nto path: %s" % (infile, outdir))
+
+    defFilesOfInterest = [
+        # Ground terrain
+        "DIRTTL.def",
+        "SANDTL.def",
+        "GRASTL.def",
+        "Snowtl.def",
+        "SWMPTL.def",
+        "ROUGTL.def",
+        "SUBBTL.def",
+        "LAVATL.def",
+        "WATRTL.def",
+        "ROCKTL.def",
+
+        # Road
+        "dirtrd.def",
+        "gravrd.def",
+        "cobbrd.def",
+
+        # River
+        "Clrrvr.def",
+        "Icyrvr.def",
+        "Mudrvr.def",
+        "Lavrvr.def",
+    ]
+
+    ret = unpack_lod(infile, outdir, outdirOfHashesFile, defFilesOfInterest)
+    if ret:
+        print("Successfully extracted LOD archive")
+    else:
+        print("Failed to extract LOD archive")
     exit(0 if ret else 1)
